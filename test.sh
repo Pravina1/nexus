ef6e849fcc1189a5f55a95d265cfca5834b2639f
Prvina1 <awatipravin10@gmail.com>
2020-11-01 14:14:31 +0530

    initial_commit

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
-git init 
-git con
 
+    git push origin HEAD:master
+    git config --global user.name "Prvina1"
+    git config --global user.email "awatipravin10@gmail.com"
+    git init 
+    echo "logs attached on $date" >>nexus_logs.txt
+    ll
+    git add --all
+    git commit -m "initial_commit"
+    git push origin HEAD:master 
