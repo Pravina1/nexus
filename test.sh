97f3e29a3285cf9366f7cde3740824fde96320b7
Prvina1 <awatipravin10@gmail.com>
2020-11-01 14:30:41 +0530

    m

--- test.sh
+++ test.sh
@@ -7,4 +7,6 @@
     ll
     git add --all
     git commit -m "initial_commit"
-    git push origin HEAD:https://pravina1@github.com/Pravina1/nexus.git 
+    git push origin HEAD:https://pravina1@github.com/Pravina1/nexus.git
+    echo "success:":w
+
