a1bb3dd1ed225a57d1b4eba325863224de2bfbf5
Pravin <46417516+Pravina1@users.noreply.github.com>
2020-11-01 14:42:06 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,6 @@
     git config --global user.email "awatipravin10@gmail.com"
     git init 
     echo "logs attached on $date" >>nexus_logs.txt
-    ll
     git add --all
     git commit -m "initial_commit"
     git push origin master 
